6ab1d68ea7361714c020892deb03aaeafa1b0fc9
Carlos Ferro <carlos.prentt.d6@gmail.com>
2022-11-22 10:42:22 -0500

    First Test

---
+++ src/test/java/screens/DashBoardScreen.java
@@ -0,0 +1,52 @@
+package screens;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.HowToUseLocators;
+import util.screens.BaseScreen;
+
+import static io.appium.java_client.pagefactory.LocatorGroupStrategy.ALL_POSSIBLE;
+
+/**
+ * DashBoard screen.
+ *
+ * @author Hans.Marquez
+ */
+public class DashBoardScreen extends BaseScreen {
+
+    /**
+     * Constructor method.
+     *
+     * @param driver : AndroidDriver
+     * @author Hans.Marquez
+     */
+    public DashBoardScreen(AndroidDriver<AndroidElement> driver) {
+        super(driver);
+    }
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"android:id/button2\")")
+    private AndroidElement dismissPreferenceUpdateButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"dismiss-icon\")")
+    private AndroidElement dismissWelcome;
+
+    @HowToUseLocators(androidAutomation = ALL_POSSIBLE)
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.disney.wdpro.dlr:id/tab_icon\").descriptionContains(\"Map\")")
+    private AndroidElement mapButton;
+
+    /**
+     * @author Hans.Marquez
+     * Navigate to Login Screen from DashBoard Screen.
+     */
+    public MapScreen goToMapScreen() {
+        if (this.isElementAvailable(dismissWelcome, 25)){
+            click(dismissWelcome);
+        }
+        if (this.isElementAvailable(dismissPreferenceUpdateButton, 25)){
+            click(dismissPreferenceUpdateButton);
+        }
+        click(mapButton);
+        return new MapScreen(driver);
+    }
+}
